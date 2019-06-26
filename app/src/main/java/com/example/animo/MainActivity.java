8a29191f4b79571af5b91a86825420a46b243c63
Parag Dangariya <dangariyaparag@gmail.com>
2019-06-26 10:42:32 -0400

    slide activity

--- app/src/main/java/com/example/animo/MainActivity.java
+++ app/src/main/java/com/example/animo/MainActivity.java
@@ -2,27 +2,50 @@ package com.example.animo;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    /*
 ImageView img;
+    */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img = findViewById(R.id.imageView);
+
+        btn = findViewById(R.id.btn);
+        /*img = findViewById(R.id.imageView);
         Animation animo = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotation);
         img.setAnimation(animo);
-
+*/
        /* View v = img;
         ObjectAnimator anim = ObjectAnimator.ofFloat(v,"rotationX",0.0f,360f);
         anim.setDuration(5000);
         anim.setRepeatCount(ValueAnimator.INFINITE);
         anim.start();*/
+
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent i = new Intent(getApplicationContext(),Main2Activity.class);
+
+               ActivityOptions options;
+
+               options =ActivityOptions.makeCustomAnimation(MainActivity.this,R.anim.slidein,R.anim.slideout);
+
+               startActivity(i,options.toBundle());
+           }
+       });
     }
 }
